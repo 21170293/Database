78f808dec71dff22427ab9e7794fce7db4431a32
Yash kumar shukla <39242081+21170293@users.noreply.github.com>
2020-12-31 12:41:52 +0530

    Create setup.py

---
+++ DB/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="DB",
+    version="0.2",
+    description="This a local DB package",
+    author="yashhake1997@gmail.com",
+    packages=["DB"],
+    install_requires=[],
+    )
